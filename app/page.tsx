771907c823b0af686bc2ccacaf1ba8f23fcf55c8
apptcion <apptcion@gmail.com>
2024-12-22 21:44:26 +0900

    change UNICORN model

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
+import {getCookie, setCookie} from '../public/js/cookie'
 
 export default function Home() {
+
+  useEffect(() => {
+    // if(getCookie('ticket') === undefined){
+    //   setCookie('from','/');
+    //   console.log("토큰 없음", getCookie('ticket'))
+    //   location.href = 'https://apptcion.site/filter';
+    // }else{
+    //   fetch('https://apptcion.site/isValid',{
+    //     method:'POST',
+    //     headers:{
+    //       'Content-Type':'application/json'
+    //     },
+    //     body:JSON.stringify({
+    //       ticket:getCookie('ticket'),
+    //     })
+    //   }).then((response) => {
+    //       if(response.ok) return response.json();
+    //   }).then((data) => {
+    //     if(!data){
+    //       setCookie('from','http://localhost:3000/');
+    //       location.href = 'https://apptcion.site/filter';
+    //       console.log("토큰 잘못됨", getCookie('ticket'))
+    //     }
+    //   })
+    // }
+  })
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
